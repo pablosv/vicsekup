055f52c0dd5c4f909e627ef0955b6254da0f219e
pablosv <pablo.no@gmail.com>
2016-08-27 09:40:38 +0200

    Created tmp and found simpler ref-program

--- vicsek.py
+++ vicsek.py
@@ -1,156 +1,188 @@
-################################################################################
-#                                                                              #
-# Copyright (c) 2014 Romain Mueller                                            #
-#                                                                              #
-# Distributed under the GNU GPL v2. For full terms see the file LICENSE.       #
-#                                                                              #
-################################################################################
+import numpy as np
+from pylab import *
+import matplotlib.pyplot as plt
+import sys
 
-import copy, random, time, datetime
-from math import pi, cos, sin, atan2, fabs, floor, ceil, sqrt
+class VicsekModel(object):
 
-# distance
-def dist(p1, p2):
-  return sqrt(fabs(p1[0]-p2[0])**2 + fabs(p1[1]-p2[1])**2)
+    def __init__(self, L=7, eta=2.0, N=300, num_steps=10, r=1.0, dt=1, v=0.03):
 
-def sign(v):
-  return v/abs(v)
+        self.L = L              # length of box
+        self.halfL = L * 0.5    # used for periodic BCs
+        self.N = N          # number of particles
+        self.eta = eta      # noise
+        self.r2 = r * r     # interaction radius squared
+        self.dt = dt        # timestep size
+        self.v = v          # magnitude of velocity
+        self.num_steps = num_steps
 
-class bucket_grid:
-  """Fixed radius nearest neighbour search using a grid of buckets of size r.
-     Width and height are needed to wrap around (periodic boundary conditions).
-    """
-  def __init__(self, points, width, height, n, m):
-    self.width = width
-    self.height = height
-    self.n = n
-    self.m = m
-    # here are your buckets (dict)
-    self.buckets = {}
-    # put all points into them
-    for p in points:
-      self.buckets.setdefault(self.get_index(p), []).append(p)
+        self.velocities = np.zeros((N, 2))
+        self.angles = np.zeros(N)
+        self.positions = np.zeros((N, 2))
 
-  def get_index(self, p):
-    return ( int(floor(p[0]/self.width*self.n)), int(floor(p[1]/self.height*self.m)) )
+        # first index is for each particle,
+        # second for which of its neighbours are in range
+        self.particles_in_range = np.eye(N, dtype=np.bool)
 
-  def neighbours(self, p, r):
-    # position of the central bucket
-    i, j = self.get_index(p)
-    # this is the number of adjacent buckets we need to check
-    cx = int(ceil(float(r)/self.width*self.n))
-    cy = int(ceil(float(r)/self.height*self.m))
-    neighbours = []
-    # check all neighbouring buckets
-    for a in range(-cx, 1+cx):
-      for b in range(-cy, 1+cy):
-        # add points
-        neighbours += filter(
-          lambda q: dist(p, q) < r,
-          self.buckets.setdefault( ( (i+a)%self.n, (j+b)%self.m ), [])
-          )
-    return neighbours
+    # initialises positions randomly in a box length L,
+    # initialises velocites in random directions with mag v
+    def initialise_experiment(self):
 
-class bird:
-  """
-  Flies.  
-  """
-  def __init__(self, app, pos, phi, speed):
-    self.pos  = pos
-    self.tail  = [ copy.deepcopy(self.pos) ]
-    self.phi   = phi
-    self.speed = speed
-    self.size  = 7
-    self.app = app
+        # initialise positions
+        self.positions = np.random.rand(N, 2) * L
 
-  def __getitem__(self, index):
-    """
-    Return position
-    """
-    return self.pos[index]
+        # generate random directions on [-pi, pi]
+        self.angles = (np.random.rand(self.N) * 2 - 1) * np.pi
+        # and use this to find velocity components in x/y directions
+        self.velocities[:, 0] = np.cos(self.angles) * self.v  
+        self.velocities[:, 1] = np.sin(self.angles) * self.v
+        # the components of velocity in x and y direction are v*cos(theta) and v*sin(theta), respectively.
 
-  def move(self):
-    self.pos = map(sum, zip(self.pos, [ self.speed*cos(self.phi), self.speed*sin(self.phi) ]))
-    # periodic boundary conditions
-    self.pos = [ self.pos[0]%app.width, self.pos[1]%app.height ] 
+    # this is where the main experiment is carried out
+    def main(self, visual_mode):
 
-class flock:
-  """
-  Many of those things together.
-  """
-  def __init__(self, app, N, r, n, speed):
-    self.N = N
-    self.r = r
-    self.n = n
-    self.app = app
-    self.speed = speed
-    # create birds
-    self.birds = [ bird(self.app, 
-                        [ random.random()*app.width, random.random()*app.height ], 
-                        2*pi*random.random(), 
-                        self.speed
-                       ) for i in range(self.N) ]
 
-  def set_temp(self, n):
-    self.n = n
-    for b in self.birds:
-      b.n = n
+        # set up plotting stuff
+        if visual_mode:
 
-  def move(self):
-    # create the buckets
-    grid = bucket_grid(self.birds, self.app.width, self.app.height, self.app.width/self.r, self.app.height/self.r)
-    # update the angles
-    for b in self.birds:
-      sin_tot = 0.
-      cos_tot = 0.
-      counter = 0
-      # loop over neighbours
-      neighbours = grid.neighbours(b, self.r)
-      for n in neighbours:
-          sin_tot += sin(n.phi)
-          cos_tot += cos(n.phi)
-      counter = len(neighbours)
-      # update
-      b.phi = atan2(sin_tot, cos_tot) + self.n/2.*(1-2.*random.random())
+            plt.ion()
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+            wframe = None
 
-    # move them
-    for b in self.birds:
-      b.move()
+            # reset positions
+            self.initialise_experiment()
 
-class game:
-  """
-  Fly, baby, fly.
-  """
-  def __init__(self):
-    self.width, self.height = 512, 512
-    self.N = 250
-    self.r = 10
-    self.n = 0.5
-    self.v = 3.
-    self.fps = 30
+            for step in xrange(self.num_steps):
 
-    self.flock = flock(self, self.N, self.r, self.n, self.v)
+                oldcol = wframe
+                wframe = self.plot_grid(ax)
+                if oldcol:
+                    ax.collections.remove(oldcol)
+                plt.draw()
 
-  def run(self):
-    running = True
+                self.perform_step()
 
-    while running:
-      # moving
-      self.flock.move()
+            print "for eta =", self.eta, " v_a =", self.find_avg_norm_vel()
 
-def test_bucket():
-  """
-  Small test function of the bucket grid
-  """
-  import matplotlib.pyplot as plt
-  points = [ ( random.random()*512, random.random()*512 ) for i in range(400) ]
-  bnn = bucket_grid(points, 512, 512, 10, 10)
-  neighbours = bnn.neighbours(points[0], 10)
+        # otherwise we are in data intensive mode
+        else:
 
-  print points[0]
-  print neighbours
+            # print header
+            print "eta\tv_a"
 
-if __name__ == "__main__":
-  #http://stackoverflow.com/questions/419163/what-does-if-name-main-do
-  app = game() 
-  app.run()
+            for self.eta in arange(0.0, 4.0, 0.2):
+
+                # reset positions
+                self.initialise_experiment()
+
+                for step in xrange(self.num_steps):
+                    self.perform_step()
+
+                print self.eta, "\t", self.find_avg_norm_vel()
+
+        print "experiment finished"
+
+    # perform one timestep
+    def perform_step(self):
+
+        # generate the set of random movements dTheta from [-eta/2, eta/2]
+        noise_increments = (np.random.rand(self.N) - 0.5) * self.eta
+
+        # find which particles are within distance r
+        self.find_particles_in_range()
+
+        # setup array for saving average directions
+        mean_directions = np.zeros(self.N)
+
+        for p in xrange(self.N):
+            # get the array of angles for particles in range of particle p
+            near_angles = self.angles[self.particles_in_range[p, :]]
+
+            # and average over these
+            mean_directions[p] = np.arctan2(np.mean(np.sin(near_angles)),
+                                            np.mean(np.cos(near_angles)))
+
+        # new direction is average of surrounding particles + noise
+        self.angles = mean_directions + noise_increments
+
+        # using these new directions, we can find the velocity
+        # vectors such that all have magnitude v
+        self.velocities[:, 0] = np.cos(self.angles) * self.v
+        self.velocities[:, 1] = np.sin(self.angles) * self.v
+        # the components of velocity in x and y direction are v*cos(theta) and v*sin(theta), respectively.
+
+        # finally we can use these to Euler-update the positions
+        self.positions = self.positions + self.velocities * self.dt
+        # apply periodic boundary conditions
+        self.positions = np.mod(self.positions, self.L)
+
+    # Updates the NxN boolean grid of whether particles are in range: first
+    # index is for each particle, second for which of its neighbours are in
+    # range. As the resulting matrix is symmetric, it doesn't really matter...
+    def find_particles_in_range(self):
+
+        # each particle is within its own range, which corresponds
+        # to the identity matrix
+        self.particles_in_range = np.eye(self.N, dtype=np.bool)
+
+        for p in xrange(self.N):
+            for q in xrange(p + 1, self.N):
+
+                # difference vector of the 2 points
+                diff = self.positions[p, :] - self.positions[q, :]
+
+                # apply minimum image criteria, i.e. use mirror
+                # image if closer
+                for dim in xrange(2):
+                    while diff[dim] > self.halfL:
+                        diff[dim] = diff[dim] - self.L
+                    while diff[dim] < -self.halfL:
+                        diff[dim] = diff[dim] + self.L
+
+
+                # get the distance squared to (avoid sqrts)
+                dist2 = np.sum(np.power(diff, 2))
+
+                # and see if this is within the allowed range
+                in_range = dist2 < (self.r2)
+                self.particles_in_range[p, q] = in_range
+                self.particles_in_range[q, p] = in_range
+
+    # find the average normalised velocity, v_a
+    def find_avg_norm_vel(self):
+
+        mean_v = np.sum(self.velocities, axis=0)
+        mag_mean_v = np.power(np.sum(np.power(mean_v, 2)), 0.5)
+
+        return mag_mean_v / (self.N * self.v)
+
+    # plot the swarm using matplotlib
+    def plot_grid(self, ax):
+        plotx = self.positions[:, 0]
+        ploty = self.positions[:, 1]
+        plotu = self.velocities[:, 0]
+        plotv = self.velocities[:, 1]
+
+        return ax.quiver(plotx, ploty, plotu, plotv)
+
+# this is the main experiment
+N, L = 150, 18
+
+rho = N / float(L * L)
+eta = 0.1
+num_steps = 100
+print "num particles:", N
+print "system size:", L
+print "density:", rho
+print "num steps:", num_steps
+
+sim = VicsekModel(L, eta, N, num_steps)
+
+
+if len(sys.argv) > 1:
+    print "using visual mode"
+    sim.main(True)
+else:
+    print "using data intensive mode"
+    sim.main(False)
